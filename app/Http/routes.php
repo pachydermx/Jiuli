7ce5aa98e62ab2aab5b5965457b2b3a25db17f86
PachydermX <renjimin@gmail.com>
2015-03-13 11:41:48 +0800

    权限控制功能

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,16 +15,27 @@ Route::get('/', 'WelcomeController@index');
 
 Route::get('home', 'HomeController@index');
 
+/* 带注册的登录
 Route::controllers([
 	'auth' => 'Auth\AuthController',
 	'password' => 'Auth\PasswordController',
 ]);
+*/
 
+//不带注册的登录
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+//PagesAdmin路由
 Route::group([
 	'prefix' => 'admin',
-	'namespace' => 'Admin'
+	'namespace' => 'Admin',
+	//权限验证
+	'middleware' => 'auth'
 	],
 	function(){
+		//由于prefix设置为admin 所以这里的/实际是admin/
 		Route::get('/', 'AdminHomeController@index');
 		Route::resource('pages', 'PagesController');
 	}
